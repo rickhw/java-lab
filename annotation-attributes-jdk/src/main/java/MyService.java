97e207d6f883096759dd07167c0ff30355d4b2be
Rick Hwang <rick_kyhwang@hotmail.com>
2024-09-27 06:25:25 +0800

    update

--- annotation-attributes-jdk/src/main/java/MyService.java
+++ annotation-attributes-jdk/src/main/java/MyService.java
@@ -1,21 +1,18 @@
-import main.java.ExecMode;
-
 public class MyService {
 
-    @DryRun
-    @Async
-    public void myMethod() {
-        System.out.println("Original method execution");
-    }
-
-    @DryRun
     @ExecMode(mode = ExecMode.Mode.ASYNC)
     public void myMethodAsync() {
         System.out.println("Original method execution");
     }
 
     @ExecMode(mode = ExecMode.Mode.SYNC)  // 明确声明同步执行
-    public void anotherMethod() {
+    public void myMethodAync() {
         System.out.println("Another method execution");
     }
+
+    @ExecMode // default sync mode.
+    public void myMethodDefault() {
+        System.out.println("default method execution");
+    }
+
 }
